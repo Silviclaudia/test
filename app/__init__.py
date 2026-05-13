69bbdc8c2e7a006cda90500d8ec2b7ac777208fe
caseyleung1 <caseyleung.au@gmail.com>
2026-05-13 12:10:40 +0800

    Added flask migration for database management

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect
 
 from config import Config
 
 
 db = SQLAlchemy()
+migrate = Migrate()
+csrf = CSRFProtect()
 login_manager = LoginManager()
 login_manager.login_view = "main.login"
 
@@ -15,11 +19,12 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
 
     db.init_app(app)
+    migrate.init_app(app, db)
+    csrf.init_app(app)
     login_manager.init_app(app)
 
-    from app import models  # noqa: F401
+    from app import models  
     from app.routes import main
-
     app.register_blueprint(main)
 
     @app.cli.command("init-db")
